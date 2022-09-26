c5c685911cc3b7424c0bc685ec47ce91eb438d92
byeongjin <qudwls7468@gmail.com>
2022-09-26 13:37:21 +0900

    Add Practices

--- 220926/day0926/urls.py
+++ 220926/day0926/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from practices import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', views.index),
+    path('ping/', views.ping),
+    path('pong/', views.pong),
 ]
